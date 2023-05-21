591fabe3d6e95849c90052fce6dfce5375670985
wandathihi <134180723+wandathihi@users.noreply.github.com>
2023-05-21 09:21:22 +0700

    datdo

---
+++ bai1.cpp
@@ -0,0 +1,56 @@
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include <iostream>
+using namespace std;
+using namespace cv;
+
+void his1(Mat src , Mat &dst)
+{
+    vector<Mat> bgr_planes;
+    split(src, bgr_planes);
+    int histSize = 256;
+    float range[] = { 0, 256 }; //the upper boundary is exclusive
+    const float* histRange[] = { range };
+    bool uniform = true, accumulate = false;
+    Mat b_hist, g_hist, r_hist;
+    calcHist(&bgr_planes[0], 1, 0, Mat(), b_hist, 1, &histSize, histRange, uniform, accumulate);
+    calcHist(&bgr_planes[1], 1, 0, Mat(), g_hist, 1, &histSize, histRange, uniform, accumulate);
+    calcHist(&bgr_planes[2], 1, 0, Mat(), r_hist, 1, &histSize, histRange, uniform, accumulate);
+    int hist_w = 512, hist_h = 300;
+    int bin_w = cvRound((double)hist_w / histSize);
+    Mat histImage(hist_h, hist_w, CV_8UC3, Scalar(0, 0, 0));
+    normalize(b_hist, b_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat());
+    normalize(g_hist, g_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat());
+    normalize(r_hist, r_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat());
+    for (int i = 1; i < histSize; i++)
+    {
+        line(histImage, Point(bin_w * (i - 1), hist_h - cvRound(b_hist.at<float>(i - 1))),
+            Point(bin_w * (i), hist_h - cvRound(b_hist.at<float>(i))),
+            Scalar(255, 0, 0), 2, 8, 0);
+        line(histImage, Point(bin_w * (i - 1), hist_h - cvRound(g_hist.at<float>(i - 1))),
+            Point(bin_w * (i), hist_h - cvRound(g_hist.at<float>(i))),
+            Scalar(0, 255, 0), 2, 8, 0);
+        line(histImage, Point(bin_w * (i - 1), hist_h - cvRound(r_hist.at<float>(i - 1))),
+            Point(bin_w * (i), hist_h - cvRound(r_hist.at<float>(i))),
+            Scalar(0, 0, 255), 2, 8, 0);
+    }
+    dst = histImage;
+}
+int main()
+{
+    Mat image1 = imread("Transformed.jpg", IMREAD_COLOR);
+    Mat image = imread("Lena.jpg",IMREAD_COLOR );
+    Mat image2 = imread("LenaBinary.jpg", IMREAD_COLOR);
+    Mat dst, src, dst1;
+    imshow("anh goc", image);
+    his1(image, src);
+    imshow("histogram", src);
+    imshow("anh xam", image1);
+    his1(image1, src);
+    imshow("histogram 2", src);  
+    his1(image2, src);
+    imshow("histogram 3", src);
+    imshow("anh nhi phan", image2);
+    waitKey(0);
+    return 0;
+}
